def937956330b4f132dc3362245aaec62ebcc51f
Marco Emrich <marco.emrich@codecentric.de>
2025-06-20 14:35:17 +0200

    exchange string calculator with example spec

--- src/string-calculator.ts
+++
@@ -1,8 +0,0 @@
-export function sumCommaSeparatedNumbers(numbers: string): number {
-  return numbers === ""
-    ? 0
-    : numbers
-        .split(",")
-        .map((n) => parseInt(n))
-        .reduce((sum, n) => sum + n, 0);
-}
